9da1ddcce0addb1a133d2d18413311251cc0e832
Tatiana-Futarnaya <tannia93@mail.ru>
2023-02-07 10:47:24 +0300

    first commit

--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -2,13 +2,13 @@ package lang.print.gaps.task2;
 
 public class CopiedNames {
     public static void main(String[] args) {
-        int variable = 0;
-        int variable = 2000;
-        int value = 1000;
-        int variable = 4000;
-        int value2 = 3000;
-        int variables = 5000;
-        variables = 7000;
+        int zero = 0;
+        int thousand2 = 2000;
+        int thousand1 = 1000;
+        int thousand4 = 4000;
+        int thousand3 = 3000;
+        int thousand5 = 5000;
+        int thousand7 = 7000;
         int value = 90000;
     }
 }
